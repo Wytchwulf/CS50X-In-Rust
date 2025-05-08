c661ab739c386975fae3cb80dc5a5d98558e8254
Sean Murdoch <145845458+Wytchwulf@users.noreply.github.com>
2025-05-08 16:51:07 +0000

    Mostly similar to mario_less

--- pset1/mario_more/src/main.rs
+++ pset1/mario_more/src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("How tall is your tower?");
+
+    let height: usize = loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Could not read line");
+
+        match input.trim().parse() {
+            Ok(num) => {
+                if num > 0 {
+                    break num;
+                } else {
+                    println!("Please enter a positive integer");
+                }
+            }
+
+            Err(_) => {
+                println!("Please enter valid type: Unsigned Integer")
+            }
+        }
+    };
+
+    for i in 1..=height {
+        println!(
+            "{}{}{}{}",
+            " ".repeat(height - i),
+            "#".repeat(i),
+            "  ",
+            "#".repeat(i)
+        );
+    }
 }
