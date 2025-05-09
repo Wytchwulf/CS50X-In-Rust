be3c324afbed11bced82840e2bc152259671da58
Sean Murdoch <145845458+Wytchwulf@users.noreply.github.com>
2025-05-09 15:32:33 +0000

    Prints the number of coins
    Need to add loop for invalid eg -1

--- pset1/cash/src/main.rs
+++ pset1/cash/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Take input from user
+    println!("Please enter total");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read line");
+
+    // Convert to f64
+    let float_val: f64 = input.trim().parse().expect("Could not parse");
+
+    // Convert to cents
+    let mut cents: u32 = (float_val * 100.0).round() as u32;
+
+    // Initialise coins list
+    let coins = [25, 10, 5, 1];
+
+    // Count the number of times cents is divided by each coin
+    let mut count: u32 = 0;
+    for coin in coins {
+        count += cents / coin;
+        cents %= coin;
+    }
+
+    println!("{} coins", count);
 }
+
