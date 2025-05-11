5228cf7c4bbb3c98dd1ac147c2ede5f26c15e54d
Sean <seanpmurdoch@proton.me>
2025-05-11 17:05:41 +0100

    Added a card type checker

--- pset1/credit/src/main.rs
+++ pset1/credit/src/main.rs
@@ -7,8 +7,38 @@ fn main() {
         .read_line(&mut input)
         .expect("Could not read line");
 
+    // Sort Card Type
+    let input = input.trim();
+
+    let card_type: &str = match input.len() {
+        13 => {
+            if &input[0..1] == "4" {
+                "VISA"
+            } else {
+                "INVALID"
+            }
+        }
+        15 => match &input[0..2] {
+            "34" => "AMEX",
+            "37" => "AMEX",
+            _ => "INVALID",
+        },
+        16 => match &input[0..1] {
+            "4" => "VISA",
+            _ => match &input[0..2] {
+                "51" => "MASTERCARD",
+                "52" => "MASTERCARD",
+                "53" => "MASTERCARD",
+                "54" => "MASTERCARD",
+                "55" => "MASTERCARD",
+                _ => "INVALID",
+            },
+        },
+        _ => "INVALID",
+    };
+
     // Convert to int
-    let long_no: u64 = match input.trim().parse() {
+    let long_no: u64 = match input.parse() {
         Ok(num) => num,
         Err(_) => {
             println!("Could not convert to integer");
@@ -16,7 +46,8 @@ fn main() {
         }
     };
 
-    println!("Test: {}", luhns(long_no));
+    // TEST
+    println!("Test: {} \n{}", luhns(long_no), card_type)
 }
 
 fn luhns(mut number: u64) -> u64 {
