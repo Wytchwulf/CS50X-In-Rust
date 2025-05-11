25244b2304f7127ce4744cfeb428921108048b44
Sean <seanpmurdoch@proton.me>
2025-05-11 15:46:41 +0100

    Added basic input and luhns algo function

--- pset1/credit/src/main.rs
+++ pset1/credit/src/main.rs
@@ -1,3 +1,40 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Take input from user
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read line");
+
+    // Convert to int
+    let long_no: u64 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Could not convert to integer");
+            return;
+        }
+    };
+
+    println!("Test: {}", luhns(long_no));
+}
+
+fn luhns(mut number: u64) -> u64 {
+    let mut sum: u64 = 0;
+    let mut double = false;
+
+    while number > 0 {
+        let mut digit = number % 10;
+
+        if double {
+            digit *= 2;
+            if digit > 9 {
+                digit -= 9;
+            }
+        }
+        sum += digit;
+        number /= 10;
+        double = !double;
+    }
+    sum
 }
