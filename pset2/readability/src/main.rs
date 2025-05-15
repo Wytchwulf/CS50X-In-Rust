a1d77c44b7f4682a37cfbb654ba471ce25a2c2ec
Sean <seanpmurdoch@proton.me>
2025-05-15 16:06:46 +0100

    user input & psudocode

--- pset2/readability/src/main.rs
+++ pset2/readability/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // Take user input
+    println!("Enter text to be graded");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read input");
+
+    // Analyse input
+
+    // Print result
 }
