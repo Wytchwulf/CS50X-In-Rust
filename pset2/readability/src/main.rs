0365565e8bdbeadd1b253dd780825afd7599939b
Sean Murdoch <145845458+Wytchwulf@users.noreply.github.com>
2025-05-16 22:17:39 +0000

    Added coleman liau logic and print results

--- pset2/readability/src/main.rs
+++ pset2/readability/src/main.rs
@@ -11,16 +11,29 @@ fn main() {
         .expect("Could not read input");
 
     // Count Chars
-    let letters = input.chars().filter(|c| c.is_alphabetic()).count();
+    let letters: f64 = input.chars().filter(|c| c.is_alphabetic()).count() as f64;
 
     // Count Words
-    let words = input.chars().filter(|c| c.is_whitespace()).count();
+    let words: f64 = input.chars().filter(|c| c.is_whitespace()).count() as f64;
 
     // Count Sentences
     let sentence_enders = ['.', '?', '!'];
-    let sentences = input.chars().filter(|c| sentence_enders.contains(c)).count();
+    let sentences: f64 = input.chars().filter(|c| sentence_enders.contains(c)).count() as f64;
 
-    println!("input = {}letters = {}\nwords = {}\nsentences= {}", input, letters, words, sentences);
+    // Avg letters in 100 words (L)
+    let l: f64 = (letters / words) * 100.0;
 
-    // Print result
+    // Avg sentences in 100 words (S)
+    let s: f64 = (sentences / words) * 100.0;
+
+    // Get grade
+    let grade = 0.0588 * l - 0.296 * s - 15.8;
+
+    if grade.round() < 1.0 {
+        println!("Before Grade 1")
+    } else if grade.round() > 16.0 {
+        println!("Grade 16+");
+    } else {
+        println!("Grade {}", grade.round());
+    }
 }
