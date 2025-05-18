acee29fbd7ea82035263e84a116f54a213fc3815
Sean Murdoch <145845458+Wytchwulf@users.noreply.github.com>
2025-05-18 20:51:49 +0000

    command line argument parsing

--- pset2/substitution/src/main.rs
+++ pset2/substitution/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "Substitution", long_about = "Substitution Based Encryption")]
+struct Args {
+    #[arg(short)]
+    key: String,
+} 
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.key.chars().count() < 26 {
+        println!("Key must be exactly 26 characters");
+        return;
+    };
+
+    println!("{} chars exactly. Well done", args.key.chars().count());
 }
