b85ef91a51ab1e9739571ac13f5ae28a18cc5c5e
Sean Murdoch <145845458+Wytchwulf@users.noreply.github.com>
2025-05-18 03:45:21 +0000

    Used clap to parse and simple if else for encryption logic

--- pset2/caesar/src/main.rs
+++ pset2/caesar/src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "Caesar", long_about = "Enter plaintext to encrypt and key")]
+struct Args {
+    #[arg(short)]
+    plaintext: String,
+
+    #[arg(short)]
+    key: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let mut ciphertext = String::new();
+
+    for char in args.plaintext.chars() {
+        if char.is_ascii_uppercase() {
+            ciphertext.push(((char as u8 - b'A' + args.key) % 26 + b'A') as char);
+        } else if char.is_ascii_lowercase() {
+            ciphertext.push(((char as u8 - b'a' + args.key) % 26 + b'a') as char);
+        } else {
+            ciphertext.push(char);
+        }
+    }
+
+    println!("Plaintext:  {}", args.plaintext);
+    println!("Ciphertext:  {}", ciphertext);
 }
