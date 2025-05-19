3ff1474dc2cbb277a8a5cb9d5ca5ce594d94c75a
Sean <seanpmurdoch@proton.me>
2025-05-19 20:33:06 +0100

    There was no code problem for this so I implemented a simple bubble sort

--- pset3/sort/src/main.rs
+++ pset3/sort/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = vec![64, 34, 25, 17, 12, 22, 1, 100];
+    sort(&mut numbers);
+    println!("{:?}", numbers);
+}
+
+fn sort(arr: &mut Vec<i32>) {
+    for i in 0..arr.len() {
+        for j in 0..(arr.len() - 1 - i) {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
 }
